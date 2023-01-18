759aebaa75ee63d00ba5cf5bab57e693fe652c4b
Polluxe <91460632+Polluxe@users.noreply.github.com>
2023-01-18 07:46:46 +0700

    15/01/2023_3

--- models/item.go
+++ models/item.go
@@ -2,6 +2,6 @@ package models
 
 type item struct {
 	Id          int64  `gorm:"primaryKey" json:"id"`
-	NamaProduct string `gorm:"type:varchar(300)" json:"nama_item"`
+	NamaItem 	string `gorm:"type:varchar(300)" json:"nama_item"`
 	Deskripsi   string `gorm:"type:text" json:"deskripsi"`
 }
